e5f572c6875febecea6a648c8ac7c6d481f505c6
Антон <31995761+Rekt1@users.noreply.github.com>
2018-12-26 21:06:40 +0300

    Delete javascript.js

--- javascript.js
+++
@@ -1,9 +0,0 @@
-var myVar = setInterval(myTimer, 1000); // Часы на главной страницу
-function myTimer() {
-	var d = new Date();
-	document.getElementById('date').innerHTML = d.toLocaleTimeString();
-}
-
-function myAlert() { // alert с сообщением
-	alert("Тестовая страница! Test page")
-}
